b8eccf674f58708ada7d751991987c35af8e5f0f
Arul <a.arulmathew9360l@gmail.com>
2026-03-24 23:09:42 +0530

    added procfile for deploy

--- app.py
+++ app.py
@@ -703,5 +703,6 @@ def download_thermal(invoice_id):
     
     return send_file(output, download_name=f"receipt_{invoice.invoice_no}.pdf", mimetype='application/pdf')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
